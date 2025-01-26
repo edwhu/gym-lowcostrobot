5eaf2ec836d358d6e171c624b9fdcb87b172d875
Edward Hu <husedward@gmail.com>
2025-01-26 15:53:04 -0500

    update lift agent

--- examples/test_blind_pick_reward.py
+++ examples/test_blind_pick_reward.py
@@ -55,39 +55,39 @@ def collect_episode(demo, env, ep):
     demo['observations']['state'].append(state)
 
     i = 0
-    print("Going up")
-    # first go up to middle of the workspace so everything is in the field of view.
-    desired_pos = np.array([0, 0.07, 0.13])
-    action = np.array([*desired_pos, -2])
-    ee_pos = env.unwrapped.get_ee_pos()
-    while np.linalg.norm(ee_pos[:3][-1] - desired_pos[-1]) > 0.02:
-        noise = np.random.normal(0, pos_action_noise, size=3)
-        rel_action = action - ee_pos
-        rel_action[:3] += noise
-        abs_action = action.copy()
-        abs_action[:3] += noise
-        rel_action = env.unwrapped.get_scaled_action(rel_action)
-        obs, reward, term, trunc, info = env.step(rel_action)
-        ee_pos = env.unwrapped.get_ee_pos()
-        demo = store_transition(demo, obs, abs_action, rel_action, reward, term, trunc, info)
+    # print("Going up")
+    # # first go up to middle of the workspace so everything is in the field of view.
+    # desired_pos = np.array([0, 0.07, 0.13])
+    # action = np.array([*desired_pos, -2])
+    # ee_pos = env.unwrapped.get_ee_pos()
+    # while np.linalg.norm(ee_pos[:3][-1] - desired_pos[-1]) > 0.02:
+    #     noise = np.random.normal(0, pos_action_noise, size=3)
+    #     rel_action = action - ee_pos
+    #     rel_action[:3] += noise
+    #     abs_action = action.copy()
+    #     abs_action[:3] += noise
+    #     rel_action = env.unwrapped.get_scaled_action(rel_action)
+    #     obs, reward, term, trunc, info = env.step(rel_action)
+    #     ee_pos = env.unwrapped.get_ee_pos()
+    #     demo = store_transition(demo, obs, abs_action, rel_action, reward, term, trunc, info)
 
-        # print(f"desired ee: {desired_pos}")
-        # print(f't:{i+1}', 'ee', ee_pos, end='\n')
-        i += 1
-        if term:
-            print(f'ep {ep} terminated with {i} actions')
-            break
-        if trunc:
-            print(f'ep {ep} truncated with {i} actions')
-            break
-    if term or trunc:
-        return demo
+    #     # print(f"desired ee: {desired_pos}")
+    #     # print(f't:{i+1}', 'ee', ee_pos, end='\n')
+    #     i += 1
+    #     if term:
+    #         print(f'ep {ep} terminated with {i} actions')
+    #         break
+    #     if trunc:
+    #         print(f'ep {ep} truncated with {i} actions')
+    #         break
+    # if term or trunc:
+    #     return demo
 
 
 
     desired_pos = info['qpos'][env.unwrapped.cube_dof_id: env.unwrapped.cube_dof_id + 3]
     # pos_diff = np.array([0.02, 0, 0.025])
-    pos_diff = np.array([0.00, 0, 0.025])
+    pos_diff = np.array([0.00, -0.02, 0.025])
     desired_pos = pos_diff + desired_pos
     action = np.array([*desired_pos, -1])
 
@@ -206,6 +206,6 @@ episodic_return = []
 episodic_success = []
 
 demos = deepcopy(demo_dict)
-for ep in range(1):
+for ep in range(100):
     demo = deepcopy(demo_dict)
     demo = collect_episode(demo, env, ep)
