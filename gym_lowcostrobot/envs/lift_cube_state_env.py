5fc5e77fbfbb4640f5e34a75993627f2326a20a6
Edward Hu <husedward@gmail.com>
2025-02-01 20:33:08 -0500

    log is success

--- gym_lowcostrobot/envs/lift_cube_state_env.py
+++ gym_lowcostrobot/envs/lift_cube_state_env.py
@@ -563,6 +563,7 @@ class LiftCubeStateDreamerV4Env(LiftCubeStateEnv):
     def __init__(self, observation_mode="state", action_mode="joint", render_mode=None, render_obs=True, include_initial_obj_pose=False):
         super().__init__(observation_mode, action_mode, render_mode, render_obs, include_initial_obj_pose)
         self.observation_subspaces = {k: v for k, v in self.observation_subspaces.items() if k in ["arm_qpos", "log_image_front", "log_is_success"]}
+        self.observation_subspaces["log/is_success"] = self.observation_subspaces.pop("log_is_success")
         if "log_image_front" in self.observation_subspaces:
             self.observation_subspaces["log/image_front"] = self.observation_subspaces.pop("log_image_front")
         self.observation_space = gym.spaces.Dict(self.observation_subspaces)
