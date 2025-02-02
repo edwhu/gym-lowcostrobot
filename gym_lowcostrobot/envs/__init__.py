05e829dfa62a022e46932ecb17bcb34cffdc9b10
Edward Hu <husedward@gmail.com>
2025-02-01 20:27:04 -0500

    dreamerv4 version env

--- gym_lowcostrobot/envs/__init__.py
+++ gym_lowcostrobot/envs/__init__.py
@@ -1,10 +1,10 @@
 from .lift_cube_env import LiftCubeEnv
 from .lift_cube_camera_env import LiftCubeCameraEnv
-from .lift_cube_state_env import LiftCubeStateEnv
+from .lift_cube_state_env import LiftCubeStateEnv, LiftCubeStateDreamerV4Env
 from .pick_place_cube_env import PickPlaceCubeEnv
 from .push_cube_env import PushCubeEnv
 from .reach_cube_env import ReachCubeEnv
 from .stack_two_cubes_env import StackTwoCubesEnv
 from .push_cube_loop_env import PushCubeLoopEnv
 
-__all__ = ["LiftCubeEnv", "LiftCubeCameraEnv", "LiftCubeStateEnv", "PickPlaceCubeEnv", "PushCubeEnv", "ReachCubeEnv", "StackTwoCubesEnv", "PushCubeLoopEnv"]
+__all__ = ["LiftCubeEnv", "LiftCubeCameraEnv", "LiftCubeStateEnv", "LiftCubeDreamerV4Env", "PickPlaceCubeEnv", "PushCubeEnv", "ReachCubeEnv", "StackTwoCubesEnv", "PushCubeLoopEnv"]
