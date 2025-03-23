acd4d03a213a0f64cdc1595d382246c02870151e
Tony Wang <tonyw3@seas.upenn.edu>
2025-03-23 17:49:48 -0400

    ADD d455 driver

--- test_mj_render.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-import gym_lowcostrobot
-
-env = gym.make("LiftCubeCameraPrivileged-v0", render_mode="rgb_array", disable_env_checker=True)
-obs, info = env.reset()
-while True:
-    obs, info = env.reset()
-    print('rendering is working')
-
